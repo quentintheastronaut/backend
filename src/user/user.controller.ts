3b40422a7411b23e4a8c16a9223ac983a9bb145c
quan.dn <quan260402@gamil.com>
2022-09-12 22:55:07 +0700

    done: API docs example

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,9 +4,10 @@ import { UserService } from './user.service';
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('User')
+@ApiBearerAuth()
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
